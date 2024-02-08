0f300aa93dd913b9267e2ad2913ceb2f58290d3a
hailey201 <122054101+hailey201@users.noreply.github.com>
2024-02-08 15:03:21 -0800

    CoyoteJumpTimer

--- player.gd
+++ player.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const ACCELERATION = 500.0
-const FRICTION = 600.0
+const SPEED = 400.0
+const ACCELERATION = 800.0
+const FRICTION = 1000.0
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
-
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 func _physics_process(delta):
 	apply_gravity(delta)
 	handle_jump()
@@ -19,17 +19,21 @@ func _physics_process(delta):
 
 	apply_friction(input_axis, delta)
 	update_animations(input_axis)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge: 
+		coyote_jump_timer.start()
 
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 func handle_jump():
-	if is_on_floor():
+	if is_on_floor or  coyote_jump_timer.time_left >0.0:
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	if not is_on_floor():
 		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2 :
 			velocity.y = JUMP_VELOCITY / 2
 
