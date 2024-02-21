ba44b1b66a63df4ae5e8b0c01a388471ae2bbfb3
hailey201 <122054101+hailey201@users.noreply.github.com>
2024-02-21 12:33:59 -0800

    ProgressCheck3

---
+++ PlayerMovementData.gd
@@ -0,0 +1,9 @@
+class_name PlayerMovementData
+extends Resource
+
+@export var speed = 400.0
+@export var acceleration = 800.0
+@export var friction = 600.0
+@export var jump_velocity = -400.0 
+@export var gravity_scale = 1.0
+@export var air_resistance = 200.0
